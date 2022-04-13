cbb84225cecaea6a35c2a60edbad2a23c294914f
Valerie Bayard <valerie.bayard@gmail.com>
2022-04-13 15:53:44 +0100

    all fixed and working

--- controllers/controller.py
+++ controllers/controller.py
@@ -11,7 +11,7 @@ def home():
 def index():
     return render_template('index.html', title="Orders", order_list = orders)
 
-@app.route('/orders/<index>')
-def display_order(index):
-    chosen_order = orders[int(index)]
+@app.route('/orders/<banana>')
+def display_order(banana):
+    chosen_order = orders[int(banana)]
     return render_template('order.html', title="Order ID", order = chosen_order)
